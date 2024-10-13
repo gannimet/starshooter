59cf8166fc3039a36936cddcd5f92af6fe31e26b
Richard Wotzlaw <r.wotzlaw@gmail.com>
2024-10-13 19:24:55 +0200

    set up key listeners for arrowup and space bar, putting active keys into keys set

--- js/input.js
+++ js/input.js
@@ -1,13 +1,25 @@
 export class InputHandler {
   constructor() {
-    this.keys = [];
+    this.keys = new Set();
 
     this.init();
   }
 
   init() {
-    window.addEventListener("keydown", function (event) {
-      console.log("key:", event.key);
+    window.addEventListener("keydown", (event) => {
+      if (event.key === " " || event.key === "ArrowUp") {
+        this.keys.add(event.key);
+      }
+
+      console.log("keys:", this.keys);
+    });
+
+    window.addEventListener("keyup", (event) => {
+      if (event.key === " " || event.key === "ArrowUp") {
+        this.keys.delete(event.key);
+      }
+
+      console.log("keys:", this.keys);
     });
   }
 }
