25272d408c51c3adfa83b73c3249409671bc48bd
Richard Wotzlaw <r.wotzlaw@gmail.com>
2024-10-18 14:46:28 +0200

    let starball fall down from top edge in parabolic motion

--- js/starball.js
+++ js/starball.js
@@ -2,12 +2,21 @@ export class Starball {
   constructor(game) {
     this.game = game;
 
-    this.size = 20;
-    this.x = this.game.width / 2;
+    this.size = 15;
+    this.x = (this.game.width * 2) / 3;
     this.y = -this.size / 2;
+    this.xSpeed = -1;
+    this.ySpeed = 0;
+    this.lastUpdateTime = 0;
   }
 
-  update() {}
+  update() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    this.xSpeed -= 0.05;
+    this.ySpeed += this.size / 350;
+    this.lastUpdateTime = performance.now();
+  }
 
   draw(ctx) {
     ctx.fillStyle = "red";
