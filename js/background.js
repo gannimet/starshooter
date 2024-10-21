0c28a1895f4b41ca0725745ffd1cacab9057fbf2
Richard Wotzlaw <r.wotzlaw@gmail.com>
2024-10-21 18:16:15 +0200

    make background scroll through infinitely

--- js/background.js
+++ js/background.js
@@ -3,11 +3,19 @@ export class Background {
     this.game = game;
 
     this.backgroundImage = document.getElementById("backgroundSky");
+    this.x = 0;
   }
 
-  update() {}
+  update() {
+    if (this.x <= -this.game.width) {
+      this.x = 0;
+    } else {
+      this.x--;
+    }
+  }
 
   draw(ctx) {
-    ctx.drawImage(this.backgroundImage, 0, 0);
+    ctx.drawImage(this.backgroundImage, this.x, 0);
+    ctx.drawImage(this.backgroundImage, this.x + this.game.width, 0);
   }
 }
