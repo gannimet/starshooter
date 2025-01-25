c878876880a6445c0fa8fb9b81da7497a9b7a0dc
Richard Wotzlaw <r.wotzlaw@gmail.com>
2025-01-25 20:27:02 +0100

    Manage starballs in a list and create them at periodic intervals

--- js/game.js
+++ js/game.js
@@ -9,17 +9,31 @@ export class Game {
 
     this.inputHandler = new InputHandler();
     this.player = new Player(this);
-    this.starball = new Starball(this);
+    this.starballs = [];
+    this.starballsPerSecond = 0.5;
+    this.lastStarballCreatedAt = 0;
   }
 
   update() {
+    const secondsSinceLastStarballCreated =
+      (performance.now() - this.lastStarballCreatedAt) / 1000;
+
+    if (secondsSinceLastStarballCreated >= 1 / this.starballsPerSecond) {
+      this.starballs.push(new Starball(this));
+      this.lastStarballCreatedAt = performance.now();
+    }
+
     this.player.update(this.inputHandler.keys);
-    this.starball.update();
+    this.starballs.forEach((starball) => {
+      starball.update();
+    });
   }
 
   draw(ctx) {
     ctx.clearRect(0, 0, this.width, this.height);
     this.player.draw(ctx);
-    this.starball.draw(ctx);
+    this.starballs.forEach((starball) => {
+      starball.draw(ctx);
+    });
   }
 }
