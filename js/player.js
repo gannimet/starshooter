67ceaf0e8fe00af4d1384d89bcc528aa0bf5155e
Richard Wotzlaw <r.wotzlaw@gmail.com>
2025-01-25 19:20:26 +0100

    Introduce enum objects for shooting state and direction, animate shooting motion on space bar press

--- js/player.js
+++ js/player.js
@@ -1,3 +1,18 @@
+const ShootingStates = {
+  IDLE: 0,
+  INTERMEDIATE_1: 1,
+  INTERMEDIATE_2: 2,
+  INTERMEDIATE_3: 3,
+  INTERMEDIATE_4: 4,
+  FULLY_EXTENDED: 5,
+};
+
+const ShootingDirections = {
+  UP: 1,
+  DOWN: -1,
+  NONE: 0,
+};
+
 export class Player {
   constructor(game) {
     this.game = game;
@@ -7,9 +22,12 @@ export class Player {
     this.playerHeight = 100;
     this.playerX = 10;
     this.playerY = (this.game.height - this.playerHeight) / 2;
+    this.shootingState = ShootingStates.IDLE;
+    this.shootingDirection = ShootingDirections.NONE;
   }
 
   update(inputKeys) {
+    // Moving up and down
     if (inputKeys.has("ArrowUp")) {
       this.playerY = Math.max(this.playerY - 3, 0);
     } else {
@@ -18,14 +36,32 @@ export class Player {
         this.game.height - this.playerHeight
       );
     }
+
+    // Shooting animation
+    if (inputKeys.has(" ") && this.shootingState === ShootingStates.IDLE) {
+      this.shootingDirection = ShootingDirections.UP;
+    }
+
+    if (
+      (this.shootingDirection === ShootingDirections.UP &&
+        this.shootingState < ShootingStates.FULLY_EXTENDED) ||
+      (this.shootingDirection === ShootingDirections.DOWN &&
+        this.shootingState > ShootingStates.IDLE)
+    ) {
+      this.shootingState += this.shootingDirection;
+    }
+
+    if (this.shootingState === ShootingStates.FULLY_EXTENDED) {
+      this.shootingDirection = ShootingDirections.DOWN;
+    } else if (this.shootingState === ShootingStates.IDLE) {
+      this.shootingDirection = ShootingDirections.NONE;
+    }
   }
 
   draw(ctx) {
-    const spriteIndex = 0;
-
     ctx.drawImage(
       this.playerSprites,
-      spriteIndex * this.playerWidth,
+      this.playerWidth * this.shootingState,
       0,
       this.playerWidth,
       this.playerHeight,
