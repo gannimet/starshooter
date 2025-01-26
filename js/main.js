1b06851522a95a7fa8c69184bb9c0df2885d1161
Richard Wotzlaw <r.wotzlaw@gmail.com>
2025-01-26 18:36:34 +0100

    Display slider for frequency of starballs underneath canvas, set frequency on game object depending on slider value

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ import { Game } from "./game.js";
 
 window.addEventListener("load", () => {
   const canvas = document.getElementById("gameCanvas");
+  const starballsSlider = document.getElementById("starballsSlider");
+  const starballsPerSecondDisplay = document.getElementById(
+    "starballsPerSecondDisplay"
+  );
   const ctx = canvas.getContext("2d");
 
   canvas.width = 800;
@@ -17,4 +21,10 @@ window.addEventListener("load", () => {
   }
 
   animate();
+
+  starballsSlider.addEventListener("change", (event) => {
+    const newFrequency = event.target.value;
+    game.starballsPerSecond = newFrequency;
+    starballsPerSecondDisplay.innerText = newFrequency;
+  });
 });
